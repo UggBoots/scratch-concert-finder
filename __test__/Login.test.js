9e1f172083c1ef02852b8c38dd2f80df7e40d560
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-12 13:24:48 -0500

    finished basic login unit tests without functionality

--- __test__/Login.test.js
+++ __test__/Login.test.js
@@ -1,56 +1,36 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toJson from 'enzyme-to-json';
+import Typography from '@material-ui/core/Typography';
 import Login from '../client/components_refactored/Login'
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-// Enzyme is a wrapper around React test utilities which makes it easier to
-// shallow render and traverse the shallow rendered tree.
-
-
-// Newer Enzyme versions require an adapter to a particular version of React
 configure({ adapter: new Adapter() });
 
-describe('Login units tests', () => {
+describe('Login React units tests', () => {
   describe('components', () => {
     let wrapper;
-    const props = {
-      label: 'Mega',
-      text: 'Markets',
-    };
 
     beforeAll(() => {
-      wrapper = shallow(<Login {...props} />);
+      wrapper = shallow(<Login/>);
     });
+	it('Renders an h1 heading with the title of Log In', () => {
+		expect(wrapper.find(Typography)).toHaveLength(1);
+		expect(wrapper.find(Typography).text()).toEqual("Log In"); 
+	  });	
 
+	it('Renders both an email and password input field', () => {
+		expect(wrapper.find(TextField)).toHaveLength(2);
+		expect(wrapper.find('#email')).toHaveLength(1);
+		expect(wrapper.find('#password')).toHaveLength(1);
+	  });
 	
     it('Renders a button with text of Log In', () => {
-	  expect(wrapper.find(Button)).toHaveLength(1)
-	  expect(wrapper.find(Button).text()).toEqual("Log In") 
-	  
-    });
-  });
+	  expect(wrapper.find(Button)).toHaveLength(1);
+	  expect(wrapper.find(Button).text()).toEqual("Log In"); 
+	  expect(wrapper.find(Button).props('style').color).toBe('primary');
+    });	
 
-  describe('MarketDisplay', () => {
-    // TODO: Test the following:
-    // 1. A MarketDisplay should display all of its text props inside a
-    // LabeledText component
-    // 2. It should also contain a div with two buttons
-    // 3. The functions passed down should be invoked on click
-    // 4. MarketDisplay should render a div with a class of `marketBox`, and the
-    // interior div wrapping the two buttons should have a class of `flex`
-
-  });
-
-  describe('MarketsDisplay', () => {
-    // TODO: Test the following:
-    //   1. A MarketsDisplay should have an h4 element to display the 'Markets'
-    //   title
-    //   2. A single MarketDisplay is rendered for each market in the
-    //   marketList prop
-    //   3. The percentage prop should be a string calculated to two decimals.
-    //   Test for zero, a whole number, and a fractional value. (Right now this
-    //   is implemented incorrectly, so follow TDD here)
   });
 });
