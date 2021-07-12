b2a1f174dcaa4820a48ab41211606c21f3b9b19e
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-12 10:41:33 -0500

    working on login testing, everything is tested besides correct username and password.  I wonder if it is a bcrypt issue.

--- __test__/verifyUser.test.js
+++ __test__/verifyUser.test.js
@@ -15,7 +15,23 @@ describe('Sign in tests', () => {
         expect(res.body.message).toBe('User does not exist');
         return;
       });
-    
+
+        it('If username field is blank respond with 400 status and user does not exist', async () => {
+          const res = await request.post('/api/signin')
+          .send({params:{password: '123123'}});
+          expect(res.status).toBe(400);
+          expect(res.body.message).toBe('User does not exist');
+          return;
+        });
+
+          it('If password field is blank respond with 500 status and unknown error', async () => {
+            const res = await request.post('/api/signin')
+            .send({params:{email:'cameron@gmail.com'}});
+            console.log(res.body.message)
+            expect(res.status).toBe(500);
+            expect(res.body.message).toBe('unknown error');
+            return;
+          });  
     });
   });
-});
+})
