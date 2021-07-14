4c142c46ef3aac111a9a9738afd38e24a4eba661
Sergey01 <Sergey01@users.noreply.github.com>
2021-07-13 22:40:58 -0400

    Fixed error handling by moving malformedRequestError function into the body of the getConcerts function.

--- server/controllers/concerts.js
+++ server/controllers/concerts.js
@@ -9,13 +9,15 @@ const axios = require('axios');
 
 const pp = (stuff) => JSON.stringify(stuff, null, 2);
 
-const malformedRequestError = () => {next({
-  log: 'Malformed request received on getConcerts middleware function',
-  status: 400,
-  message: 'Malformed request - missing required parameters'
-})};
+
 
 const getConcerts = async (req, res, next) => {
+  const malformedRequestError = () => {next({
+    log: 'Malformed request received on getConcerts middleware function',
+    status: 400,
+    message: 'Malformed request - missing required parameters'
+  })};
+  
   // console.log(`req.body = ${pp(req.body)}`)
   // if (!(lng, lat, date in req.body)) {
     if (!('lng', 'lat' in req.body)) {
