99faee94fb78d3569cd53ff8aaa74847817e30ee
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-13 20:20:47 -0500

    randomized email for cypress test

--- cypress/integration/user-interaction-tests/user.spec.js
+++ cypress/integration/user-interaction-tests/user.spec.js
@@ -1,12 +1,24 @@
+import Chance from 'chance'
+
 describe('My first test', () => {
+	
+	const chance = new Chance()
+	const name = chance.name();
+	const email = chance.email();
+	const password = 'abce1234';
+	
+
 	before(() => {
 		// Cypress starts out with a blank slate for each test
 		// so we must tell it to visit our website with the `cy.visit()` command.
 		// Since we want to visit the same URL at the start of all our tests,
 		// we include it in our beforeEach function so that it runs before each test
 		cy.visit('http://localhost:8080/')
+		
 	  })
 	
+	
+
 	it('Find an infinity logo and click it', () => {
 		cy.get('.MenuButton').should('have.length', 1);
 		cy.get('.MenuButton').click();
@@ -18,13 +30,13 @@ describe('My first test', () => {
 	it('Register a new user with a name, email, and password', () => {
 		cy.get('.regName').should('have.length', 1);
 		cy.get('.regName').click();
-		cy.get('.regName').type('Eric');
+		cy.get('.regName').type(name);
 		cy.get('.regEmail').should('have.length', 1);
 		cy.get('.regEmail').click();
-		cy.get('.regEmail').type('Eric123@gmail.com');
+		cy.get('.regEmail').type(email);
 		cy.get('.regPW').should('have.length', 1);
 		cy.get('.regPW').click();
-		cy.get('.regPW').type('apple');
+		cy.get('.regPW').type(password);
 		cy.get('[id^=regC]').should('have.length', 1);
 		cy.get('[id^=regC]').click();
 		cy.get('html').clickOutside();
@@ -36,10 +48,10 @@ describe('My first test', () => {
 		cy.get('[id^=login]').should('have.length', 1);
 		cy.get('[id^=login]').click();
 		cy.get('.logEmail').click();
-		cy.get('.logEmail').type('Eric123@gmail.com');
+		cy.get('.logEmail').type(email);
 		cy.get('.logPW').should('have.length', 1);
 		cy.get('.logPW').click();
-		cy.get('.logPW').type('apple');
+		cy.get('.logPW').type(password);
 		cy.get('[id^=logC]').should('have.length', 1);
 		cy.get('[id^=logC]').click();
 		cy.get('html').clickOutside();
