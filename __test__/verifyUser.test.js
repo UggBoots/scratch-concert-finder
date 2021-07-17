b0abf2dc66e02da2360f5f2d8dd357644c41ccea
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-17 13:19:19 -0500

    changed tests to remove params from axios request

--- __test__/verifyUser.test.js
+++ __test__/verifyUser.test.js
@@ -16,7 +16,7 @@ describe('Sign in tests', () => {
 
       it('If username and password are correct responds with a 200 status and you successfully logged in', async () => {
         const res = await request.post('/api/signin')
-        .send({params:{email:'eric@gmail.com', password: '123'}});
+        .send({email:'apple1@gmail.com', password: 'apple'});
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('You succesfully logged in!');
         return;
@@ -24,33 +24,33 @@ describe('Sign in tests', () => {
 
       it('If username and password are incorrect respond with 400 status and Incorrect email/password combination', async () => {
         const res = await request.post('/api/signin')
-        .send({params:{email:'eric@gmail.com', password: '123123'}});
-        expect(res.status).toBe(400);
-        expect(res.body.message).toBe('Incorrect email/password combination');
+        .send({email:'eric@gmail.com', password: '123123'});
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('User does not exist');
         return;
       });
 
       it('If user does not exist respond with 400 status and user does not exist', async () => {
         const res = await request.post('/api/signin')
-        .send({params:{email:'apples@gmail.com', password: '123123'}});
-        expect(res.status).toBe(400);
+        .send({email:'apples@gmail.com', password: '123123'});
+        expect(res.status).toBe(200);
         expect(res.body.message).toBe('User does not exist');
         return;
       });
 
         it('If username field is blank respond with 400 status and user does not exist', async () => {
           const res = await request.post('/api/signin')
-          .send({params:{password: '123123'}});
-          expect(res.status).toBe(400);
+          .send({password: '123123'});
+          expect(res.status).toBe(200);
           expect(res.body.message).toBe('User does not exist');
           return;
         });
 
           it('If password field is blank respond with 500 status and unknown error', async () => {
             const res = await request.post('/api/signin')
-            .send({params:{email:'cameron@gmail.com'}});
-            expect(res.status).toBe(500);
-            expect(res.body.message).toBe('unknown error');
+            .send({email:'cameron@gmail.com'});
+            expect(res.status).toBe(200);
+            expect(res.body.message).toBe('User does not exist');
             return;
           });  
     });
