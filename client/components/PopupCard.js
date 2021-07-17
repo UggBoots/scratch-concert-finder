2a5f0e3bdd81c51ec0f19d53099c1b21d56497a9
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-07-17 16:22:14 -0400

    Renamed components_refactored to components

--- client/components/PopupCard.js
+++ client/components/PopupCard.js
@@ -8,6 +8,16 @@ import {
   ClickAwayListener,
 } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 const PopupCard = ({
   addFav,
@@ -20,7 +30,7 @@ const PopupCard = ({
 }) => {
   // if (selectedConcert.description) selectedConcert.description = selectedConcert.description.replace('\r\n', '&#10;').replace('\n', '&#10;');
   // console.log(`selectedConcert content: ${JSON.stringify(selectedConcert,null,2)}`)
-  if (selectedConcert.description) selectedConcert.description = 'Details not available';
+  if (selectedConcert.description == "") selectedConcert.description = 'Details not available';
   const [isFav, setIsFav] = useState(false);
 
   const handleClick = () => {
